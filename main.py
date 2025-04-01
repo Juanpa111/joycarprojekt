14e3a5d6c30edf19648af8024e5787e034447279
Juan Pablo <123707426+Juanpa111@users.noreply.github.com>
2025-03-31 21:01:29 -0500

    mode auf null gesetzt und als global Variable. Hinter die Linefinder Logik wurde durch eine Funktion mit spezifischen Bedingungen eingesetzt. is True/False als Booleans, wenn ein Sensor an oder ausgemacht ist.

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-def linefinder():
-    if JoyCar.linefinder(SensorLCRSelection.LEFT) and ( not JoyCar.linefinder(SensorLCRSelection.CENTER) and JoyCar.linefinder(SensorLCRSelection.RIGHT)):
-        JoyCar.drive(FRLRDirection.FORWARD, 10)
-
-    elif JoyCar.linefinder(SensorLCRSelection.LEFT) and (JoyCar.linefinder(SensorLCRSelection.CENTER) and JoyCar.linefinder(SensorLCRSelection.RIGHT)):
-        JoyCar.drive(FRLRDirection.RIGHT, 5)
-    
+mode = 0
 
 def on_button_pressed_a():
-    linefinder()
+    global mode
+    mode += 1
+    if mode == 1:
+        linefinder()
+    elif mode == 0:
+        basic.show_leds("""
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            """)
+        JoyCar.stop(StopIntensity.INTENSE)
+    else:
+        basic.show_leds("""
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            """)
+        JoyCar.brakelight(ToggleSwitch.ON)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def linefinder():
+    if JoyCar.linefinder(SensorLCRSelection.LEFT) is True and (JoyCar.linefinder(SensorLCRSelection.CENTER) is False and (JoyCar.linefinder(SensorLCRSelection.RIGHT) is True)):
+        JoyCar.drive(FRLRDirection.FORWARD, 20)
+    elif JoyCar.linefinder(SensorLCRSelection.LEFT) is True and (JoyCar.linefinder(SensorLCRSelection.CENTER) is True and (JoyCar.linefinder(SensorLCRSelection.RIGHT) is False)):
+        JoyCar.drive(FRLRDirection.RIGHT, 10)
+    elif JoyCar.linefinder(SensorLCRSelection.LEFT) is False and (JoyCar.linefinder(SensorLCRSelection.CENTER) is True and ( JoyCar.linefinder(SensorLCRSelection.RIGHT) is True)):
+        JoyCar.drive(FRLRDirection.LEFT, 10)
+    elif JoyCar.linefinder(SensorLCRSelection.LEFT) is True and (JoyCar.linefinder(SensorLCRSelection.CENTER) is True and ( JoyCar.linefinder(SensorLCRSelection.RIGHT) is True)):
+        JoyCar.drive(FRLRDirection.REVERSE, 5)
 def on_button_pressed_b():
     JoyCar.stop(StopIntensity.INTENSE)
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
--- main.ts
+++ main.ts
@@ -1,15 +1,43 @@
+let mode = 0
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    mode += 1
+    if (mode == 1) {
+        linefinder()
+    } else if (mode == 0) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        JoyCar.stop(StopIntensity.Intense)
+    } else {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            `)
+        JoyCar.brakelight(ToggleSwitch.On)
+    }
+    
+})
 function linefinder() {
-    if (JoyCar.linefinder(SensorLCRSelection.Left) && (!JoyCar.linefinder(SensorLCRSelection.Center) && JoyCar.linefinder(SensorLCRSelection.Right))) {
-        JoyCar.drive(FRLRDirection.Forward, 10)
-    } else if (JoyCar.linefinder(SensorLCRSelection.Left) && (JoyCar.linefinder(SensorLCRSelection.Center) && JoyCar.linefinder(SensorLCRSelection.Right))) {
-        JoyCar.drive(FRLRDirection.Right, 5)
+    if (JoyCar.linefinder(SensorLCRSelection.Left) === true && (JoyCar.linefinder(SensorLCRSelection.Center) === false && JoyCar.linefinder(SensorLCRSelection.Right) === true)) {
+        JoyCar.drive(FRLRDirection.Forward, 20)
+    } else if (JoyCar.linefinder(SensorLCRSelection.Left) === true && (JoyCar.linefinder(SensorLCRSelection.Center) === true && JoyCar.linefinder(SensorLCRSelection.Right) === false)) {
+        JoyCar.drive(FRLRDirection.Right, 10)
+    } else if (JoyCar.linefinder(SensorLCRSelection.Left) === false && (JoyCar.linefinder(SensorLCRSelection.Center) === true && JoyCar.linefinder(SensorLCRSelection.Right) === true)) {
+        JoyCar.drive(FRLRDirection.Left, 10)
+    } else if (JoyCar.linefinder(SensorLCRSelection.Left) === true && (JoyCar.linefinder(SensorLCRSelection.Center) === true && JoyCar.linefinder(SensorLCRSelection.Right) === true)) {
+        JoyCar.drive(FRLRDirection.Reverse, 5)
     }
     
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    linefinder()
-})
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     JoyCar.stop(StopIntensity.Intense)
 })
